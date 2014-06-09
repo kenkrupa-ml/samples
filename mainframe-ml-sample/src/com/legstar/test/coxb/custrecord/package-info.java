3b29c8cff69f2681e4f670530f317bf85b17cc09
Ken Krupa <ken.krupa@marklogic.com>
2014-06-08 20:21:03 -0400

    cleanup

--- mainframe-ml-sample/src/com/legstar/test/coxb/custrecord/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://coxb.test.legstar.com/custrecord", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.legstar.test.coxb.custrecord;
